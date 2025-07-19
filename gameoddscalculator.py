e298d03254a97bb75fa666ab44875f4871462768
Brayden <93243935+BraydoRaptor@users.noreply.github.com>
2025-07-19 23:40:57 +0930

    Update gameoddscalculator.py with Minecraft Looting Modifiers

--- gameoddscalculator.py
+++ gameoddscalculator.py
@@ -64,6 +64,88 @@ class TerrariaModifier(GameOddsModifier):
     def __printdisclaimer(self, luck:float) -> None:
         print(f"== {TerrariaDisclamer}, the luck you inputted was {luck} ==")
 
+class MinecraftModifier(GameOddsModifier):
+    '''
+        This will set of the chains
+        it also returns the new odds based on a formula on your luck number
+    '''
+
+    # Minecraft Exclusive Statements
+    MinecraftOddsExplanation = ["Minecraft has mob drops and fishing as their two primary rng rolling mechanics", "Looting, which has three levels can affect the chance of rolls as well as increase the number of rolls"]
+    MinecraftDisclamer = "Minecraft has so many items, especially common and uncommon mod drops, fishing is completely different and depends on different lure mechanics which are currently not explored"
+    rare_string = "R"
+    common_string = "C"
+
+
+    def adjustOdds(self, current_odds:float) -> float:
+        self.__handleExplanation()
+        lootingNumber = self.__promptForLooting("looting")
+        self.__printdisclaimer(lootingNumber)
+        user_item_type = self.__promptItemType()
+        if(user_item_type == self.rare_string):
+            influence_number = 1
+            return current_odds / 100 + lootingNumber * influence_number
+        elif(user_item_type == self.common_string):
+            current_odds = self.__handleCommonDrops(lootingNumber, current_odds)
+            return current_odds
+
+        return current_odds
+    '''
+        Prompts for looting 0-3 based on a string
+    '''
+    def __promptForLooting(self, change_cond:str) -> int:
+        user_looting = 0
+        min_looting = 0
+        max_looting = 3
+        while user_looting >= min_looting and user_looting <= max_looting:
+            try: 
+                user_looting = int(input(f"Enter the number of {change_cond} you had: "))
+                break
+            except ValueError:
+                print(f"Invalid input, enter a number between {min_looting} and {max_looting}")
+        return user_looting
+    '''
+        helper to print out what needs to be explaned about minecraft looting
+    '''
+    def __handleExplanation(self) -> None:
+        for sentence in self.MinecraftOddsExplanation:
+            print(f"| {sentence}")
+
+    '''
+        handles the martix boost of odds that apply with looting using a reference matrix (or a list of dictionaries)
+    '''
+
+    def __handleCommonDrops(self,looting:int, current_odds:float) -> int:
+        current_odds = current_odds /100 # assume success
+        loot_alteration_matrix = [{0:0.5,1:0.5,2:0,3:0}, {0:0.25,1:0.5,2:0.25,3:0}, {0:0.17,1:0.33,2:0.33,3:0.17}]
+        index = 0
+        current_drops = self.__promptForLooting("average drops")
+        while index < len(loot_alteration_matrix):
+            if(looting == index+1):
+                for num_modifier in loot_alteration_matrix[index]:
+                    if(num_modifier == current_drops):
+                        return current_odds + current_odds*loot_alteration_matrix[index][num_modifier]
+            index=index+1
+        return 0 # not possible odds
+                
+                
+    '''
+        prompts for the type of mod drop which will differ based on looting
+    '''
+    def __promptItemType(self) -> str:
+        
+        invalid_input = True
+        while invalid_input:
+            type_item = input(f"Enter whether {self.common_string} (common or uncommon drops) or {self.rare_string} (rare drops): ").upper() # uppercase to handle both upper and lower cases
+            if(type_item.startswith(self.common_string) or type_item.startswith(self.rare_string)): # startswith in case the user types out the full word
+                invalid_input = False
+            else:
+                print("INVALID INPUT")
+        return type_item
+    def __printdisclaimer(self, looting:int) -> None:
+        print(f"== {self.MinecraftDisclamer}, the looting you inputted was {looting} ==")
+
+
 class ProbabilityGenerator():
     # fail and success strings are predefined here so the system detecting what is valid input can be changed with ease
     fail_string = "f"
@@ -139,10 +221,10 @@ class ProbabilityGenerator():
     def __calculateFinalOdds(self, odds:float, times:int) -> float:
         return odds ** times # to the power of
 if __name__ == "__main__":  
-    knownGames = ["terraria"]
+    knownGames = ["terraria", "minecraft"]
     print("The current known games with odd modifiers are the following:")
-    for _game in knownGames:
-        print(f"{_game}")
+    for _index, _game in enumerate(knownGames):
+        print(f"{_index}: {_game}")
     invalid_input = True
     while invalid_input:
         game = input("Which game would you like to choose? ").lower()
@@ -150,7 +232,11 @@ if __name__ == "__main__":
             invalid_input = False
         else:
             print("INVALID INPUT, please reference the available games")
+    newGenerator = ProbabilityGenerator(game, TerrariaModifier())
     match game:
         case "terraria":  
-            newGenerator = ProbabilityGenerator(game, TerrariaModifier())
+            newGenerator.modifier = TerrariaModifier()
+            newGenerator.askForOdds() # Starts the chain of events in the program
+        case "minecraft":
+            newGenerator.modifier = MinecraftModifier()
             newGenerator.askForOdds() # Starts the chain of events in the program
